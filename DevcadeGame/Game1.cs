702008eed1b42d5340184f9a64672093bf9d359b
Andrew Ebersole <ane5279@rit.edu>
2023-02-23 13:52:31 -0500

    Created a menu and some helper variables

--- DevcadeGame/Game1.cs
+++ DevcadeGame/Game1.cs
@@ -10,6 +10,38 @@ namespace DevcadeGame
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
 
+		// Game State
+		private enum GameState
+        {
+			Menu,
+			Game,
+			GameOver,
+			Instructions,
+			Credits
+        }
+		private GameState gameState;
+
+		// Window Dimensions
+		private int windowHeight;
+		private int windowWidth;
+		private int windowTileSize;
+
+		// Menu Selector
+		private int menuSelectorPos;
+
+		// Texture 2Ds
+		Texture2D singleColor;
+
+		// Fonts
+		SpriteFont arial24;
+
+		// Colors
+		private Color[] menuColors;
+
+		// Keyboard State
+		KeyboardState currentKB;
+		KeyboardState previousKB;
+
 		/// <summary>
 		/// Game constructor
 		/// </summary>
@@ -39,8 +71,29 @@ namespace DevcadeGame
 			_graphics.ApplyChanges();
 #endif
 			#endregion
-			
-			// TODO: Add your initialization logic here
+
+			// Set gamestate
+			gameState = GameState.Menu;
+
+			// Window Dimensions
+			windowHeight = _graphics.PreferredBackBufferHeight;
+			windowWidth = _graphics.PreferredBackBufferWidth;
+			windowTileSize = windowWidth / 7;
+
+			// Create single Color Texture
+			singleColor = new Texture2D(GraphicsDevice, 1, 1);
+			singleColor.SetData(new Color[] { Color.White });
+
+			// Menu Colors
+			menuColors = new Color[5];
+			for (int i = 0; i < menuColors.Length; i++)
+            {
+				menuColors[i] = Color.Black;
+            }
+			menuColors[0] = Color.Gold;
+
+			// Menu Coords
+			menuSelectorPos = 0;
 
 			base.Initialize();
 		}
@@ -52,9 +105,8 @@ namespace DevcadeGame
 		{
 			_spriteBatch = new SpriteBatch(GraphicsDevice);
 
-			// TODO: use this.Content to load your game content here
-			// ex.
-			// texture = Content.Load<Texture2D>("fileNameWithoutExtention");
+			arial24 = Content.Load<SpriteFont>("arial-24");
+
 		}
 
 		/// <summary>
@@ -75,9 +127,94 @@ namespace DevcadeGame
 				Exit();
 			}
 
-			// TODO: Add your update logic here
+			currentKB = Keyboard.GetState();
 
-			base.Update(gameTime);
+			// Game Finite State Machine
+			#region GameState Update FSM
+			switch (gameState)
+            {
+				// Menu ---------------------------------------------
+				case GameState.Menu:
+
+					// change the selected text
+					if (currentKB.IsKeyDown(Keys.Down) && previousKB.IsKeyUp(Keys.Down)
+						&& menuSelectorPos < 2)
+                    {
+						menuSelectorPos++;
+                    }
+					if (currentKB.IsKeyDown(Keys.Up) && previousKB.IsKeyUp(Keys.Up)
+						&& menuSelectorPos > 0)
+					{
+						menuSelectorPos--;
+					}
+
+					// Select next gameState
+					if (currentKB.IsKeyDown(Keys.Enter) && previousKB.IsKeyUp(Keys.Enter))
+					{
+						switch (menuSelectorPos)
+                        {
+							case 0:
+								gameState = GameState.Game;
+								break;
+
+							case 1:
+								gameState = GameState.Instructions;
+								break;
+
+							case 2:
+								gameState = GameState.Credits;
+                                break;
+                        }
+					}
+
+					// Change selected text to gold all others to black
+					for (int i = 0; i < menuColors.Length; i++)
+					{
+						menuColors[i] = Color.Black;
+					}
+					menuColors[menuSelectorPos] = Color.Gold;
+
+					break;
+
+				// Instructions -------------------------------------
+				case GameState.Instructions:
+					if (currentKB.IsKeyDown(Keys.Enter) && previousKB.IsKeyUp(Keys.Enter))
+					{
+						gameState = GameState.Menu;
+					}
+					break;
+
+				// Game ---------------------------------------------
+				case GameState.Game:
+
+					// TODO: PUT THE GAME STUFF HERE
+
+					if (currentKB.IsKeyDown(Keys.Enter) && previousKB.IsKeyUp(Keys.Enter))
+					{
+						gameState = GameState.GameOver;
+					}
+					break;
+
+				// GameOver -----------------------------------------
+				case GameState.GameOver:
+					if (currentKB.IsKeyDown(Keys.Enter) && previousKB.IsKeyUp(Keys.Enter))
+					{
+						gameState = GameState.Menu;
+					}
+					break;
+
+				// Credits ------------------------------------------
+				case GameState.Credits:
+					if (currentKB.IsKeyDown(Keys.Enter) && previousKB.IsKeyUp(Keys.Enter))
+					{
+						gameState = GameState.Menu;
+					}
+					break;
+            }
+			#endregion
+
+			previousKB = currentKB;
+            base.Update(gameTime);
 		}
 
 		/// <summary>
@@ -86,13 +223,92 @@ namespace DevcadeGame
 		/// <param name="gameTime">This is the gameTime object you can use to get the time since last frame.</param>
 		protected override void Draw(GameTime gameTime)
 		{
-			GraphicsDevice.Clear(Color.CornflowerBlue);
+			GraphicsDevice.Clear(Color.White);
 
 			_spriteBatch.Begin();
-			// TODO: Add your drawing code here
+
+			#region GameState Draw FSM
+			switch (gameState)
+			{
+				// Menu ---------------------------------------------
+				case GameState.Menu:
+					DrawMenu(_spriteBatch);
+					break;
+
+				// Instructions -------------------------------------
+				case GameState.Instructions:
+					DrawInstructions(_spriteBatch);
+
+					break;
+
+				// Game ---------------------------------------------
+				case GameState.Game:
+					DrawGame(_spriteBatch);
+					break;
+
+				// GameOver -----------------------------------------
+				case GameState.GameOver:
+					DrawGameOver(_spriteBatch);
+					break;
+
+				// Credits ------------------------------------------
+				case GameState.Credits:
+					DrawCredits(_spriteBatch);
+					break;
+			}
+			#endregion
+
 			_spriteBatch.End();
 
 			base.Draw(gameTime);
 		}
+
+        #region Draw GameState Methods
+        private void DrawMenu(SpriteBatch sb)
+        {
+			sb.DrawString(
+				arial24,											// Font
+				"Connect 7!",										// Text
+				new Vector2(windowTileSize, 1 * windowTileSize),	// Position
+				Color.Black);                                       // Color
+
+			sb.DrawString(
+				arial24,
+				"Start Game",
+				new Vector2(2 * windowTileSize, 4 * windowTileSize),
+				menuColors[0]);
+
+			sb.DrawString(
+				arial24,
+				"How to Play",
+				new Vector2(2 * windowTileSize, 6 * windowTileSize),
+				menuColors[1]);
+			sb.DrawString(
+				arial24,
+				"Credits",
+				new Vector2(2 * windowTileSize, 8 * windowTileSize),
+				menuColors[2]);
+		}
+
+		private void DrawInstructions(SpriteBatch sb)
+        {
+
+        }
+
+		private void DrawGame(SpriteBatch sb)
+        {
+
+        }
+
+        private void DrawGameOver(SpriteBatch sb)
+        {
+
+        }
+
+		private void DrawCredits(SpriteBatch sb)
+        {
+
+        }
+		#endregion
 	}
 }
